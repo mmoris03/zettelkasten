6430aa93d3d26db587785bdd08b02465bf596e51
mmoris03 <miguel.moris.gomez@gmail.com>
2025-11-19 14:33:24 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mmoris03.github.io/zettelkasten",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
